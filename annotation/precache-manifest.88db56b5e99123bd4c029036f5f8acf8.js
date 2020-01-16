beaf008237458f160490e297c94085c42b952f71
zhaochang <zhaochan@usc.edu>
2020-01-16 12:20:19 -0800

    annotation

---
+++ annotation/precache-manifest.88db56b5e99123bd4c029036f5f8acf8.js
@@ -0,0 +1,486 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d75ccab0e7bd95457b7",
+    "url": "/simulation/annotation/css/app.368f6ebe.css"
+  },
+  {
+    "revision": "a90bda42ec7b9b658cf9",
+    "url": "/simulation/annotation/css/chunk-vendors.d39e9fad.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/simulation/annotation/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/simulation/annotation/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9ca72b2c7ad95fc8a020484f674b7b96",
+    "url": "/simulation/annotation/img/all.9ca72b2c.png"
+  },
+  {
+    "revision": "5d0f0ddd2e5eab5a307bbd580aad24f3",
+    "url": "/simulation/annotation/img/bg.5d0f0ddd.jpg"
+  },
+  {
+    "revision": "270243c99d087f8d0a8f7631a3527d27",
+    "url": "/simulation/annotation/img/done.270243c9.png"
+  },
+  {
+    "revision": "c1203ae787bd8f4086c26b863eec7efc",
+    "url": "/simulation/annotation/index.html"
+  },
+  {
+    "revision": "0d75ccab0e7bd95457b7",
+    "url": "/simulation/annotation/js/app.2741a4a5.js"
+  },
+  {
+    "revision": "a90bda42ec7b9b658cf9",
+    "url": "/simulation/annotation/js/chunk-vendors.8b3077a7.js"
+  },
+  {
+    "revision": "0b8dfd3fc9b82b73f9ffb2cc6fa29a6e",
+    "url": "/simulation/annotation/layui/css/layui.css"
+  },
+  {
+    "revision": "c9a1460b219fedbf1d26ec5d92afe92f",
+    "url": "/simulation/annotation/layui/css/layui.mobile.css"
+  },
+  {
+    "revision": "eb41fb72cf46ee0f786ce194055acfda",
+    "url": "/simulation/annotation/layui/css/modules/code.css"
+  },
+  {
+    "revision": "5dc3eb5e80e81012b4cebea9fb52c150",
+    "url": "/simulation/annotation/layui/css/modules/laydate/default/laydate.css"
+  },
+  {
+    "revision": "ba81b24c06e2e0eac1e219405b33766b",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/icon-ext.png"
+  },
+  {
+    "revision": "551539f873d9ebe0792b120a9867d399",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/icon.png"
+  },
+  {
+    "revision": "020bc66b2be098e2d22b291139b5f08e",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/layer.css"
+  },
+  {
+    "revision": "a72011ccdc2bcd23ba440f104c416193",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/loading-0.gif"
+  },
+  {
+    "revision": "1140bc5c7863f8e54a3c2b179e640758",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/loading-1.gif"
+  },
+  {
+    "revision": "50c5e3e79b276c92df6cc52caeb464f0",
+    "url": "/simulation/annotation/layui/css/modules/layer/default/loading-2.gif"
+  },
+  {
+    "revision": "16a9a2babf772c15df2506162d48be8c",
+    "url": "/simulation/annotation/layui/font/iconfont.eot"
+  },
+  {
+    "revision": "e1a599b12d9ac7ddc127c908620d918c",
+    "url": "/simulation/annotation/layui/font/iconfont.svg"
+  },
+  {
+    "revision": "7c864ff719f95f49b174da4fae2a8d98",
+    "url": "/simulation/annotation/layui/font/iconfont.ttf"
+  },
+  {
+    "revision": "96b672621418a0f8fe229b7ad4e06f43",
+    "url": "/simulation/annotation/layui/font/iconfont.woff"
+  },
+  {
+    "revision": "0a602623a896914b1096ec7c0c69472e",
+    "url": "/simulation/annotation/layui/font/iconfont.woff2"
+  },
+  {
+    "revision": "a06a2e3ed6da3796fbb731367d039a0a",
+    "url": "/simulation/annotation/layui/images/face/0.gif"
+  },
+  {
+    "revision": "913e2f3dd1962d2a80b4ae66ddddceb3",
+    "url": "/simulation/annotation/layui/images/face/1.gif"
+  },
+  {
+    "revision": "611117291370fea2ceac62c29e3895bd",
+    "url": "/simulation/annotation/layui/images/face/10.gif"
+  },
+  {
+    "revision": "199be3fbe9ed7e5191c8635e05bcc0e9",
+    "url": "/simulation/annotation/layui/images/face/11.gif"
+  },
+  {
+    "revision": "7621ac2727678687a5318762f580142e",
+    "url": "/simulation/annotation/layui/images/face/12.gif"
+  },
+  {
+    "revision": "8621f66098b5c352706832525ca98430",
+    "url": "/simulation/annotation/layui/images/face/13.gif"
+  },
+  {
+    "revision": "c30514bf7d87214840be9409e0543aa9",
+    "url": "/simulation/annotation/layui/images/face/14.gif"
+  },
+  {
+    "revision": "485a20018f6fd278510c2953697ba65c",
+    "url": "/simulation/annotation/layui/images/face/15.gif"
+  },
+  {
+    "revision": "3d4ee858ebba0e46a7850e13185b7c9b",
+    "url": "/simulation/annotation/layui/images/face/16.gif"
+  },
+  {
+    "revision": "1e345b472299bbc72d854fea14377b19",
+    "url": "/simulation/annotation/layui/images/face/17.gif"
+  },
+  {
+    "revision": "f81ed31a2829c0609354f25e1da62006",
+    "url": "/simulation/annotation/layui/images/face/18.gif"
+  },
+  {
+    "revision": "fa7da45a4cf11818476a64b11972beba",
+    "url": "/simulation/annotation/layui/images/face/19.gif"
+  },
+  {
+    "revision": "aec972de4da971327e4452c5b4b5fda1",
+    "url": "/simulation/annotation/layui/images/face/2.gif"
+  },
+  {
+    "revision": "435dbb7f69e76dbe51c64d0208478bd5",
+    "url": "/simulation/annotation/layui/images/face/20.gif"
+  },
+  {
+    "revision": "21ed3c01c99c75162cc7d5c09e557468",
+    "url": "/simulation/annotation/layui/images/face/21.gif"
+  },
+  {
+    "revision": "7873504cf41c6bf81dc385e7a63e06c2",
+    "url": "/simulation/annotation/layui/images/face/22.gif"
+  },
+  {
+    "revision": "84b3bd065735379c7ebf902bd356eb24",
+    "url": "/simulation/annotation/layui/images/face/23.gif"
+  },
+  {
+    "revision": "9044159f1635cce276f79f2750e44bab",
+    "url": "/simulation/annotation/layui/images/face/24.gif"
+  },
+  {
+    "revision": "55ff58341b23eb412186f8e89963f3ed",
+    "url": "/simulation/annotation/layui/images/face/25.gif"
+  },
+  {
+    "revision": "7e64a36433e1f756dafc74093e71c678",
+    "url": "/simulation/annotation/layui/images/face/26.gif"
+  },
+  {
+    "revision": "4e10558193648444470bb9a1dd7007da",
+    "url": "/simulation/annotation/layui/images/face/27.gif"
+  },
+  {
+    "revision": "ed40b41d05a3020271be545a607d6d78",
+    "url": "/simulation/annotation/layui/images/face/28.gif"
+  },
+  {
+    "revision": "4fb6439d891b34c4936ae34a79725b59",
+    "url": "/simulation/annotation/layui/images/face/29.gif"
+  },
+  {
+    "revision": "bf97be0b15ace15dedf22f266a5c429c",
+    "url": "/simulation/annotation/layui/images/face/3.gif"
+  },
+  {
+    "revision": "0b7ff8f1bbb91be880ef190767774c78",
+    "url": "/simulation/annotation/layui/images/face/30.gif"
+  },
+  {
+    "revision": "dcc636d88b53e73852db566a4d9d2f20",
+    "url": "/simulation/annotation/layui/images/face/31.gif"
+  },
+  {
+    "revision": "8d9e9cc9b52ca46e854480fa4900158e",
+    "url": "/simulation/annotation/layui/images/face/32.gif"
+  },
+  {
+    "revision": "2f9e45312f49e02f32ce853db930295f",
+    "url": "/simulation/annotation/layui/images/face/33.gif"
+  },
+  {
+    "revision": "f81a7f4f93f254d58c0e943a07d2cc0b",
+    "url": "/simulation/annotation/layui/images/face/34.gif"
+  },
+  {
+    "revision": "cc8ff0383ce624c0721682aaa500b672",
+    "url": "/simulation/annotation/layui/images/face/35.gif"
+  },
+  {
+    "revision": "645be9a08ae4d8aac61e6b5fb47215e5",
+    "url": "/simulation/annotation/layui/images/face/36.gif"
+  },
+  {
+    "revision": "4c9e106e702751cb61861778269e2b26",
+    "url": "/simulation/annotation/layui/images/face/37.gif"
+  },
+  {
+    "revision": "15bd343617ec5ea02eedfdfe575c656c",
+    "url": "/simulation/annotation/layui/images/face/38.gif"
+  },
+  {
+    "revision": "edfe69e1b1f20cfa8110d9e9d2536c68",
+    "url": "/simulation/annotation/layui/images/face/39.gif"
+  },
+  {
+    "revision": "4d2933c0445dc04bdaaad41f2b557137",
+    "url": "/simulation/annotation/layui/images/face/4.gif"
+  },
+  {
+    "revision": "c94db1a006caf85ccb8a194335ce5621",
+    "url": "/simulation/annotation/layui/images/face/40.gif"
+  },
+  {
+    "revision": "55565f0156566feef1e329177f6d83f1",
+    "url": "/simulation/annotation/layui/images/face/41.gif"
+  },
+  {
+    "revision": "f24d53dee3bd1050b26d6d7cd5bca68d",
+    "url": "/simulation/annotation/layui/images/face/42.gif"
+  },
+  {
+    "revision": "599c45d8a2832cc01617fb42091993df",
+    "url": "/simulation/annotation/layui/images/face/43.gif"
+  },
+  {
+    "revision": "b0f285b595f10a1277774bf6844da76b",
+    "url": "/simulation/annotation/layui/images/face/44.gif"
+  },
+  {
+    "revision": "d17783318c1587204b6155c2fa9986fe",
+    "url": "/simulation/annotation/layui/images/face/45.gif"
+  },
+  {
+    "revision": "b72ecabbadc13888d4775ec53c9150f8",
+    "url": "/simulation/annotation/layui/images/face/46.gif"
+  },
+  {
+    "revision": "8ede6bd1d173ddeb6f3f4b241c8f3513",
+    "url": "/simulation/annotation/layui/images/face/47.gif"
+  },
+  {
+    "revision": "2a4c6936a09b0219d1c74f0167577b30",
+    "url": "/simulation/annotation/layui/images/face/48.gif"
+  },
+  {
+    "revision": "21069e4a6b8a4f6a0af40c87a168f321",
+    "url": "/simulation/annotation/layui/images/face/49.gif"
+  },
+  {
+    "revision": "8ae6b6ec8d2941a6adaee9555839e81c",
+    "url": "/simulation/annotation/layui/images/face/5.gif"
+  },
+  {
+    "revision": "96664d71d30c946747bcc651d085ed7a",
+    "url": "/simulation/annotation/layui/images/face/50.gif"
+  },
+  {
+    "revision": "c6c1e6c64c16d3747e251200e014f219",
+    "url": "/simulation/annotation/layui/images/face/51.gif"
+  },
+  {
+    "revision": "ff4d93b3615d5d975f0b6786670b702f",
+    "url": "/simulation/annotation/layui/images/face/52.gif"
+  },
+  {
+    "revision": "bc06dab3c63c4e2300c5cd4524819370",
+    "url": "/simulation/annotation/layui/images/face/53.gif"
+  },
+  {
+    "revision": "aff05197849e8c8f55b2d5fe56bb56f7",
+    "url": "/simulation/annotation/layui/images/face/54.gif"
+  },
+  {
+    "revision": "d7bdca562fd0b53f816eedb23148e158",
+    "url": "/simulation/annotation/layui/images/face/55.gif"
+  },
+  {
+    "revision": "8c6f0b4cdd0fa9d68205bab3d8df29e4",
+    "url": "/simulation/annotation/layui/images/face/56.gif"
+  },
+  {
+    "revision": "e37c81739515617cf5bc51232ad873b7",
+    "url": "/simulation/annotation/layui/images/face/57.gif"
+  },
+  {
+    "revision": "89d291439d6eae4b2db6746e589f6134",
+    "url": "/simulation/annotation/layui/images/face/58.gif"
+  },
+  {
+    "revision": "1cb4f698dd69602dd6f6eff121663a17",
+    "url": "/simulation/annotation/layui/images/face/59.gif"
+  },
+  {
+    "revision": "9946d6232e406ba2c10b60221c5b698b",
+    "url": "/simulation/annotation/layui/images/face/6.gif"
+  },
+  {
+    "revision": "71712fa565d89315b6ef2a45b3d581b5",
+    "url": "/simulation/annotation/layui/images/face/60.gif"
+  },
+  {
+    "revision": "4bde9630ca80c61063813274729af4ba",
+    "url": "/simulation/annotation/layui/images/face/61.gif"
+  },
+  {
+    "revision": "c0ed0920ba0d752ad77aca762050b12d",
+    "url": "/simulation/annotation/layui/images/face/62.gif"
+  },
+  {
+    "revision": "907b3e81d16afb9df5ef023ede0eddf1",
+    "url": "/simulation/annotation/layui/images/face/63.gif"
+  },
+  {
+    "revision": "959bacfef9ac0d3bb275504623c62375",
+    "url": "/simulation/annotation/layui/images/face/64.gif"
+  },
+  {
+    "revision": "a27ed9871926a8e37c522f6c1542df9a",
+    "url": "/simulation/annotation/layui/images/face/65.gif"
+  },
+  {
+    "revision": "02709c83458b72b0c73585297cc291f3",
+    "url": "/simulation/annotation/layui/images/face/66.gif"
+  },
+  {
+    "revision": "ab1c7c7a43aa6f43963c7b0afc9ec787",
+    "url": "/simulation/annotation/layui/images/face/67.gif"
+  },
+  {
+    "revision": "a1c8f21f98fc6eff21cb3e4d08baee2b",
+    "url": "/simulation/annotation/layui/images/face/68.gif"
+  },
+  {
+    "revision": "877ff95213ae5c45721761c540810053",
+    "url": "/simulation/annotation/layui/images/face/69.gif"
+  },
+  {
+    "revision": "3880bad4694b3ef38e2e58be68b875ef",
+    "url": "/simulation/annotation/layui/images/face/7.gif"
+  },
+  {
+    "revision": "b7f69e6df691b1c885f541ec604c59df",
+    "url": "/simulation/annotation/layui/images/face/70.gif"
+  },
+  {
+    "revision": "55a42691f8560bf2fbfed7c19389e4cf",
+    "url": "/simulation/annotation/layui/images/face/71.gif"
+  },
+  {
+    "revision": "8bfcb8fe88a3b666b5460743fb2cdc49",
+    "url": "/simulation/annotation/layui/images/face/8.gif"
+  },
+  {
+    "revision": "92bf7127158347196c4f9aef0d0ab301",
+    "url": "/simulation/annotation/layui/images/face/9.gif"
+  },
+  {
+    "revision": "2a6caa3eaea045f81bf7a0ef39246c12",
+    "url": "/simulation/annotation/layui/lay/modules/carousel.js"
+  },
+  {
+    "revision": "9f37dfaec45ad00602a87448f2400b0c",
+    "url": "/simulation/annotation/layui/lay/modules/code.js"
+  },
+  {
+    "revision": "0b38ffc03041ecfe8460c35878c7a187",
+    "url": "/simulation/annotation/layui/lay/modules/colorpicker.js"
+  },
+  {
+    "revision": "df4f6f1cdfa2693735a32e87d43d1a71",
+    "url": "/simulation/annotation/layui/lay/modules/element.js"
+  },
+  {
+    "revision": "25ab339f3cfbf9a198297fb665cba94d",
+    "url": "/simulation/annotation/layui/lay/modules/flow.js"
+  },
+  {
+    "revision": "e754011805a731a705d2d441152bffcf",
+    "url": "/simulation/annotation/layui/lay/modules/form.js"
+  },
+  {
+    "revision": "cb2def920947c8ba5703b070905554c2",
+    "url": "/simulation/annotation/layui/lay/modules/jquery.js"
+  },
+  {
+    "revision": "f5cca83f34b5b56a99c9218341d5a728",
+    "url": "/simulation/annotation/layui/lay/modules/laydate.js"
+  },
+  {
+    "revision": "13979c86d24bf9b49155d0ce4ff4096e",
+    "url": "/simulation/annotation/layui/lay/modules/layedit.js"
+  },
+  {
+    "revision": "6294f5dbc08611b55542ee8ffe47dbed",
+    "url": "/simulation/annotation/layui/lay/modules/layer.js"
+  },
+  {
+    "revision": "d274209e6ed192c3bc53bdb5edbc83a2",
+    "url": "/simulation/annotation/layui/lay/modules/laypage.js"
+  },
+  {
+    "revision": "e351fb5122cfdc72e62b0539bc9109c4",
+    "url": "/simulation/annotation/layui/lay/modules/laytpl.js"
+  },
+  {
+    "revision": "e351b3f5fcaeb7ad325a2b9a3cc453f4",
+    "url": "/simulation/annotation/layui/lay/modules/mobile.js"
+  },
+  {
+    "revision": "a475e6e5d2b29858cec06ec9203be550",
+    "url": "/simulation/annotation/layui/lay/modules/rate.js"
+  },
+  {
+    "revision": "c195205605c472eb03e0d43c174398c3",
+    "url": "/simulation/annotation/layui/lay/modules/slider.js"
+  },
+  {
+    "revision": "302df7833bd9e7216e602923ec2da552",
+    "url": "/simulation/annotation/layui/lay/modules/table.js"
+  },
+  {
+    "revision": "df2bf92369819d2beba1ba64c5caf305",
+    "url": "/simulation/annotation/layui/lay/modules/transfer.js"
+  },
+  {
+    "revision": "e05ae637bb3a4f705fd2336eda59a8cb",
+    "url": "/simulation/annotation/layui/lay/modules/tree.js"
+  },
+  {
+    "revision": "19e2609ca87f7424b54b4713ab3300ca",
+    "url": "/simulation/annotation/layui/lay/modules/upload.js"
+  },
+  {
+    "revision": "53d49c7c2496bfdbc12ed10bbd39a922",
+    "url": "/simulation/annotation/layui/lay/modules/util.js"
+  },
+  {
+    "revision": "43c3263dd2ad262c35dde403b980ee07",
+    "url": "/simulation/annotation/layui/layui.all.js"
+  },
+  {
+    "revision": "e40241ab3c80340d0587b0277e352f63",
+    "url": "/simulation/annotation/layui/layui.js"
+  },
+  {
+    "revision": "51475879e9ed8e7a1360ec662b8f7b3a",
+    "url": "/simulation/annotation/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simulation/annotation/robots.txt"
+  }
+]);
